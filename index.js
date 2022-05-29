0f001e57acb7fb8c5276b9c6e1a2ebf8e26992a9
Prasanga09 <74712372+Prasanga09@users.noreply.github.com>
2022-05-29 18:18:39 +0545

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ function passcheck() {
     alert('Correct password. Click OK to enter webpage');
   }
 }
+document.onkeydown=function(e)
+{
+  if(event.keyCode == 123)
+  {
+    return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keycode == 'I'.charCodeAt(0))
+  {
+    return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keycode == 'J'.charCodeAt(0))
+  {
+    return false;
+  }
+  if(e.ctrlKey && e.keycode == 'U'.charCodeAt(0))
+  {
+    return false;
+  }
+}
