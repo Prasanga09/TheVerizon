584ef543ab5fb179b21a4a094345eb2eb1ceabc6
Prasanga09 <74712372+Prasanga09@users.noreply.github.com>
2022-05-29 17:59:12 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var password = "ilikethiccgirls";
+function passcheck() {
+  if (document.getElementById('bruh').value != password} {
+        alert('Wrong password, Try Again, Hint: askyourmom.');
+  }
+  if (document.getElementById('bruh').value == password} {
+        alert('Correct password. Click OK to enter webpage');
+  }
+}
